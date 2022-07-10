9f44ae218025f3b17a2fac2167655b7299d375b8
Emmanuel Ramon <emmanuelrlima@gmail.com>
2022-07-10 15:56:11 -0300

    Banco de dados H2 e criacao automatica

--- src/main/java/com/ramown/cursomc/domain/Category.java
+++ src/main/java/com/ramown/cursomc/domain/Category.java
@@ -1,10 +1,17 @@
 package com.ramown.cursomc.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Category implements Serializable {
     private static Long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
 
